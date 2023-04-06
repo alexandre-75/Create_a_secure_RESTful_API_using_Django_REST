3e62c293a7d10647203938a3c18f449813d4b096
sassy alexandre <sassy.alexandre@gmail.com>
2023-04-06 11:22:03 +0200

    settings

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+
 
 class User(AbstractUser):
-    
-    pass
 
     def __str__(self):
-            return {self.username}
+        return self.username
+
+    pass
--- source/project/models.py
+++ source/project/models.py
@@ -5,10 +5,10 @@ class Project(models.Model):
 
     TYPE = [("BACK", "Back-end"), ("FRONT", "Front-end"), ("IOS", "iOS"), ("ANDROID", "Android")]
 
-    title = models.CharField(verbose_name="project_title", max_length=255)
-    description = models.CharField(verbose_name="project_description", max_length=2048)
-    type = models.CharField(verbose_name="project_type", max_length=50, choices=TYPE)
-    author_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=2048)
+    type = models.CharField(max_length=50, choices=TYPE)
+    author_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
 
     class Meta:
         ordering = ['id']
@@ -23,11 +23,12 @@ class Contributor(models.Model):
 
     permission = models.CharField(verbose_name="contributor_permission", choices=PERMISSION, max_length=50)
     role = models.CharField(verbose_name="contributor_role", max_length=255)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_id')
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project_id')
 
     class Meta:
         ordering = ['id']
+        unique_together = ['user_id', 'project_id']
 
 
 class Issue(models.Model):
@@ -36,18 +37,21 @@ class Issue(models.Model):
     PRIORITY = [("LOW", "Faible"), ("MEDIUM", "Moyenne"), ("HIGH", "Elevée")]
     STATUS = [("TODO", "A faire"), ("DEV", "En cours"), ("DONE", "Terminé")]
 
-    title = models.CharField(verbose_name="issue_title",max_length=255)
-    description = models.CharField(verbose_name="issue_description", max_length=2048)
-    tag = models.CharField(verbose_name="issue_tag", max_length=6, choices=TAG)
-    priority = models.CharField(verbose_name="issue_priority", max_length=6, choices=PRIORITY)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=2048)
+    tag = models.CharField(max_length=6, choices=TAG)
+    priority = models.CharField(max_length=6, choices=PRIORITY)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    status = models.CharField(verbose_name="issue_status", max_length=4, choices=STATUS)
-    author_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    assignee_user = models.ForeignKey(User,on_delete=models.CASCADE, default=author_user)
+    status = models.CharField(max_length=4, choices=STATUS)
+    author_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_user')
+    assignee_user = models.ForeignKey(User,on_delete=models.CASCADE, default=author_user, related_name='assignee_user')
     created_datetime = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['created_datetime']
+    
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
