4d4c8c7779953f95b51b6be679bd5c4471cb3217
sassy alexandre <sassy.alexandre@gmail.com>
2023-04-06 15:06:46 +0200

    ProjectDetail(RetrieveAPIView, UpdateAPIView, DestroyAPIView)

--- source/project/urls.py
+++ source/project/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from project.views import ProjectList
+from project.views import ProjectList, ProjectDetail
 
 
 urlpatterns = [
     path('', ProjectList.as_view()),
+    path('<int:pk>', ProjectDetail.as_view()),
 ]
 
--- source/project/views.py
+++ source/project/views.py
@@ -1,7 +1,12 @@
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from .models import Project
 from .serializers import ProjectSerializer
 
 class ProjectList(ListAPIView, CreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    
+    
+class ProjectDetail(RetrieveAPIView, UpdateAPIView, DestroyAPIView): 
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
